0a9c09f22dccb2ef9b37aa18b2dbfa33125fcbf1
Zajaczkowski23 <krystian14714@gmail.com>
2024-02-11 20:54:25 +0100

    created cors connection to communicate with my app

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Profile = require('../models/profile.js')
+const cors = require('cors')
 
 const app = express()
 const AuthRoute = require('../routes/auth.js')
 
 const dbURL = 'mongodb+srv://krystian:krystian2@cluster.c6yyhpd.mongodb.net/livescore?retryWrites=true&w=majority'
 const port = 4200;
+const corsOptions = {
+  origin: 'http://localhost:3000'
+};
 
+app.use(cors(corsOptions))
 app.use(express.json());
-
 app.use('/api', AuthRoute)
 
 mongoose.connect(dbURL).then((result) => {
